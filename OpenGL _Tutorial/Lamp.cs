60615336ebc976033882a424780cb961251e985c
madzixon <magdalenapierzchala0@gmail.com>
2024-02-06 18:40:37 +0100

    Lamp begining

---
+++ OpenGL _Tutorial/Lamp.cs
@@ -0,0 +1,80 @@
+﻿using Assimp;
+using OpenTK.Graphics.OpenGL4;
+using OpenTK.Mathematics;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace OpenGL__Tutorial
+{
+    public class Lamp
+    {
+        private Shader shader;
+        Vector3 lampPos;
+        public Lamp(Shader shader, Vector3 lampPos)
+        {
+            this.shader = shader;
+            this.lampPos = lampPos;
+
+            float[] cubeVertices =
+            {
+                // Position
+                -0.5f, -0.5f, -0.5f, // Front face
+                 0.5f, -0.5f, -0.5f,
+                 0.5f,  0.5f, -0.5f,
+                 0.5f,  0.5f, -0.5f,
+                -0.5f,  0.5f, -0.5f,
+                -0.5f, -0.5f, -0.5f,
+
+                -0.5f, -0.5f,  0.5f, // Back face
+                 0.5f, -0.5f,  0.5f,
+                 0.5f,  0.5f,  0.5f,
+                 0.5f,  0.5f,  0.5f,
+                -0.5f,  0.5f,  0.5f,
+                -0.5f, -0.5f,  0.5f,
+
+                -0.5f,  0.5f,  0.5f, // Left face
+                -0.5f,  0.5f, -0.5f,
+                -0.5f, -0.5f, -0.5f,
+                -0.5f, -0.5f, -0.5f,
+                -0.5f, -0.5f,  0.5f,
+                -0.5f,  0.5f,  0.5f,
+
+                 0.5f,  0.5f,  0.5f, // Right face
+                 0.5f,  0.5f, -0.5f,
+                 0.5f, -0.5f, -0.5f,
+                 0.5f, -0.5f, -0.5f,
+                 0.5f, -0.5f,  0.5f,
+                 0.5f,  0.5f,  0.5f,
+
+                -0.5f, -0.5f, -0.5f, // Bottom face
+                 0.5f, -0.5f, -0.5f,
+                 0.5f, -0.5f,  0.5f,
+                 0.5f, -0.5f,  0.5f,
+                -0.5f, -0.5f,  0.5f,
+                -0.5f, -0.5f, -0.5f,
+
+                -0.5f,  0.5f, -0.5f, // Top face
+                 0.5f,  0.5f, -0.5f,
+                 0.5f,  0.5f,  0.5f,
+                 0.5f,  0.5f,  0.5f,
+                -0.5f,  0.5f,  0.5f,
+                -0.5f,  0.5f, -0.5f
+            };
+
+            int vboLamp = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ArrayBuffer, vboLamp);
+            GL.BufferData(BufferTarget.ArrayBuffer, cubeVertices.Length * sizeof(float), cubeVertices, BufferUsageHint.StaticDraw);
+
+            int vaoLamp = GL.GenVertexArray();
+            GL.BindVertexArray(vaoLamp);
+
+            var vertexLocation = shader.GetAttribLocation("aPos");
+            GL.EnableVertexAttribArray(vertexLocation);
+            GL.VertexAttribPointer(vertexLocation, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
+        }
+    }
+    
+}
